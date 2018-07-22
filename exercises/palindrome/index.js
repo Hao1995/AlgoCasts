8e7de6f2ed6c06b96c8cf6904e8e76e761e7a4e3
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-07-22 19:26:51 +0800

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,17 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    const strArr = str.split('');
+    const len = strArr.length;
+    var flag = true;
+    for (var i = 0; i < len / 2; i++) {
+        if (i < len - i && strArr[i] != strArr[len-i-1]){
+            flag = false;
+        }
+    }
+    return flag;
+}
+
 
 module.exports = palindrome;
