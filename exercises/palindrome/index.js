bb0fee4817c2fa28034fea059f505a2e55d46ac6
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-07-22 19:40:56 +0800

    palindrome official

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,30 +8,30 @@
 //   palindrome("abcdefg") === false
 
 // //===By Myself
-function palindrome(str) {
-    const strArr = str.split('');
-    const len = strArr.length;
-    var flag = true;
-    for (var i = 0; i < len / 2; i++) {
-        if (strArr[i] != strArr[len-i-1]){
-            flag = false;
-        }
-    }
-    return flag;
-}
-
-// //===Official-1
 // function palindrome(str) {
 //     const strArr = str.split('');
 //     const len = strArr.length;
 //     var flag = true;
 //     for (var i = 0; i < len / 2; i++) {
-//         if (i < len - i && strArr[i] != strArr[len-i-1]){
+//         if (strArr[i] != strArr[len-i-1]){
 //             flag = false;
 //         }
 //     }
 //     return flag;
 // }
+
+// //===Official-1
+// function palindrome(str) {
+//     const reversed = str.split('').reverse().join('');
+//     return str === reversed;
+// }
+
+//===Official-2
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+        return char === str[str.length - i - 1];
+    }); 
+}
 
 
 module.exports = palindrome;
