93eb9601adeb4daa53efbec5cdef8a502a8efd01
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-07-22 17:17:33 +0800

    official answer

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,17 +6,46 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-//---By Myself
+//===By Myself
+// function reverse(str) {
+//     var splitStrArr = str.split("");
+//     var reverseStrArr = [];
+//     for (i=0; i<splitStrArr.length; i++)   {
+//         reverseStrArr[splitStrArr.length-i] = splitStrArr[i];
+//     }
+
+//     var reverseStr = reverseStrArr.join("");
+//     return reverseStr
+// }
+
+//===Official-1
+// function reverse(str) {
+//     const arr = str.split('');
+//     arr.reverse();
+//     return arr.join('');
+// }
+
+//===Official-2
+// function reverse(str) {
+//     return str
+//         .split('')
+//         .reverse()
+//         .join('');
+// }
+
+// //===Official-3
+// function reverse(str) {
+//     let reversed = '';
+//     for (let character of str){
+//         reversed = character + reversed;
+//     }
+
+//     return reversed;
+// }
+
+//===Official-4
 function reverse(str) {
-    var splitStrArr = str.split("");
-    var reverseStrArr = [];
-    for (i=0; i<splitStrArr.length; i++)   {
-        reverseStrArr[splitStrArr.length-i] = splitStrArr[i];
-    }
-
-    var reverseStr = reverseStrArr.join("");
-
-    return reverseStr
+    return str.split('').reduce((rev, char) => char + rev, '');
 }
 
 module.exports = reverse;
