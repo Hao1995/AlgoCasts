6b28e431e171f5735ea2b218832ff3df852ca667
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-07-28 09:42:52 +0800

    maxChar -- by myself

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// //===By Myself
+// function maxChar(str) {
+//     const chars = {};
+
+//     for (let char of str) {
+//         chars[char] = chars[char] + 1 || 1;
+//     }
+
+//     var maxIdx = null;
+//     var maxValue = 0;
+//     for (let idx in chars) {
+
+//         var count = chars[idx];
+//         if (!maxIdx) {
+//             maxIdx = idx;
+//         }
+//         if (count > maxValue) {
+//             maxIdx = idx;
+//             maxValue = count;
+//         }
+//     }
+
+//     return maxIdx;
+// }
+
 
 module.exports = maxChar;
