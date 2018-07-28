baa70ccdd4658a6bb275300926ef766ff601087b
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-07-28 09:47:25 +0800

    maxChar official

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -30,5 +30,27 @@
 //     return maxIdx;
 // }
 
+//===Official-1
+function maxChar(str) {
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        if (charMap[char]) {
+            charMap[char]++;
+        }else{
+            charMap[char] = 1;
+        }
+    }
+
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+    return maxChar;
+}
 
 module.exports = maxChar;
