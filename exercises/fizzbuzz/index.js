cdc37d26a8780d392cf1c7cd700c1cd171dce7aa
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-07-28 10:02:41 +0800

    fizzbuzz by myself

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,21 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+//=== By Myself
+function fizzBuzz(n) {
+    let tmp = 1;
+    while (tmp <= n) {
+        if (tmp% 5 === 0 && tmp% 3 === 0 ) {
+            console.log('fizzbuzz');
+        }else if (tmp% 3 === 0) {
+            console.log('fizz');
+        }else if (tmp% 5 === 0) {
+            console.log('buzz');
+        }else {
+            console.log(tmp);
+        }
+        tmp++;
+    }
+}
 
 module.exports = fizzBuzz;
