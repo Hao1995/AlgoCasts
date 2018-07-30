b3390685aa7164f5710cf4349e0367e4c168c5c6
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-07-30 12:21:31 +0800

    anagrams - official-1

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,33 +8,62 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-//=== by myself
-function anagrams(stringA, stringB) {
-    const charMapA = countChar(stringA.replace(/[^\w]/g,"").toLowerCase());
-    const charMapB = countChar(stringB.replace(/[^\w]/g,"").toLowerCase());
+// //=== by myself
+// function anagrams(stringA, stringB) {
+//     const charMapA = countChar(stringA.replace(/[^\w]/g,"").toLowerCase());
+//     const charMapB = countChar(stringB.replace(/[^\w]/g,"").toLowerCase());
 
-    if (Object.keys(charMapA).length === Object.keys(charMapB).length){
-        for (let char in charMapA) {
-            if (charMapA[char] !== charMapB[char]){
-                return false;
-            }
-        }
-        return true;
+//     if (Object.keys(charMapA).length === Object.keys(charMapB).length){
+//         for (let char in charMapA) {
+//             if (charMapA[char] !== charMapB[char]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     return false;
+// }
+
+// function countChar(str) {
+//     const charMap = {};
+//     let max = 0;
+
+//     for (let char of str) {
+//         if (charMap[char]) {
+//             charMap[char]++;
+//         }else{
+//             charMap[char] = 1;
+//         }
+//     }
+
+//     return charMap;
+// }
+
+
+//=== Official-1
+function anagrams(stringA, stringB) {
+    const aCharMap = buildCharMap(stringA);
+    const bCharMap = buildCharMap(stringB);
+
+    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+        return false;
     }
 
-    return false;
+    for (let char in aCharMap) {
+        if (aCharMap[char] !== bCharMap[char]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-function countChar(str) {
+function buildCharMap(str) {
     const charMap = {};
-    let max = 0;
 
-    for (let char of str) {
-        if (charMap[char]) {
-            charMap[char]++;
-        }else{
-            charMap[char] = 1;
-        }
+    for (let char of str.replace(/[^\w]/g,"").toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
     }
 
     return charMap;
