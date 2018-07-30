8a3845c91da8f11aa1ad340aceb78f2ab5473a05
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-07-30 14:35:30 +0800

    capitalize - official

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,19 +7,45 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-//=== by myself
-function capitalize(str) {
-    let sliceMap = str.split('');
+// //=== by myself
+// function capitalize(str) {
+//     let sliceMap = str.split('');
 
-    for (let i = 0; i < sliceMap.length; i++) {
-        if (i === 0) {
-            sliceMap[i] = sliceMap[i].toUpperCase();
-        } else if (sliceMap[i-1] === ' ' && sliceMap[i] !== ' ') {
-            sliceMap[i] = sliceMap[i].toUpperCase();
+//     for (let i = 0; i < sliceMap.length; i++) {
+//         if (i === 0) {
+//             sliceMap[i] = sliceMap[i].toUpperCase();
+//         } else if (sliceMap[i-1] === ' ' && sliceMap[i] !== ' ') {
+//             sliceMap[i] = sliceMap[i].toUpperCase();
+//         }
+//     }
+
+//     return sliceMap.join('');
+// }
+
+// //=== official-01
+// function capitalize(str) {
+//     const words = [];
+
+//     for (let word of str.split(' ')) {
+//         words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+
+//     return words.join(' ');
+// }
+
+//=== official-02
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+
+    for (let i = 1; i < str.length; i++) {
+        if ( str[i - 1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
         }
     }
 
-    return sliceMap.join('');
+    return result;
 }
 
 module.exports = capitalize;
