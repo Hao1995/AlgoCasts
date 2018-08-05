4d025837fc49ec5dc372a9e7b0b44f12b196d551
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-08-05 21:09:21 +0800

    steps -- official-2-2 recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -47,7 +47,27 @@
 //     }
 // }
 
-//=== Official-2
+// //=== Official-2
+// function steps(n, row = 0, stair = '') {
+//     if (n === row) {
+//         return;
+//     }
+
+//     if (n === stair.length) {
+//         console.log(stair);
+//         return steps(n, row + 1);
+//     }
+
+//     if (stair.length <= row) {
+//         stair += '#';
+//     } else {
+//         stair += ' ';
+//     }
+
+//     steps(n, row, stair);
+// }
+
+//=== Official-2-2
 function steps(n, row = 0, stair = '') {
     if (n === row) {
         return;
@@ -58,13 +78,8 @@ function steps(n, row = 0, stair = '') {
         return steps(n, row + 1);
     }
     
-    if (stair.length <= row) {
-        stair += '#';
-    } else {
-        stair += ' ';
-    }
-
-    steps(n, row, stair);
+    const add = stair.length <= row ? '#' : ' ';
+    steps(n, row, stair + add);
 }
 
 module.exports = steps;
