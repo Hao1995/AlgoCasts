afe1974002ea74eacbe35bf73b606a3d658ba03d
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-08-05 20:52:13 +0800

    steps official-1

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -18,17 +18,32 @@
 //       '####'
 
 
-//=== By Myself
+// //=== By Myself
+// function steps(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let str = '';
+//         for (let j = i; j > 0; j--) {
+//             str += '#';            
+//         }
+//         for (let j = n - i; j > 0; j--) {
+//             str += ' ';
+//         }
+//         console.log(str)
+//     }
+// }
+
+//=== Official-1
 function steps(n) {
-    for (let i = 1; i <= n; i++) {
-        let str = '';
-        for (let j = i; j > 0; j--) {
-            str += '#';            
+    for (let row = 0; row < n; row++) {
+        let stair = '';
+        for (let column = 0; column < n; column++) {
+            if (column <= row) {
+                stair += '#';
+            } else {
+                stair += ' ';
+            }
         }
-        for (let j = n - i; j > 0; j--) {
-            str += ' ';
-        }
-        console.log(str)
+        console.log(stair);
     }
 }
 
