75ead1da5e67e0a15c11dc5f68d948a82b0542f8
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-08-10 16:27:52 +0800

    vowels - official-2

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -23,18 +23,24 @@
 //     return vowelCount;
 // }
 
-// === official -1
+// // === official -1
+// function vowels(str) {
+//     let count = 0;
+//     const checker = ['a', 'e', 'i', 'o', 'u'];    
+
+//     for (let char of str.toLowerCase()) {
+//         if (checker.includes(char)) {
+//             count++;
+//         }
+//     }
+
+//     return count;
+// }
+
+// === official -2
 function vowels(str) {
-    let count = 0;
-    const checker = ['a', 'e', 'i', 'o', 'u'];    
-
-    for (let char of str.toLowerCase()) {
-        if (checker.includes(char)) {
-            count++;
-        }
-    }
-
-    return count;
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
