066ba526a3118c0eb480c6db7665860cb1183355
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-08-11 11:49:29 +0800

    fibonacci -- official02

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -20,17 +20,26 @@
 //     return fib(n-1) + fib(n-2);
 // }
 
-//=== Officail01
-function fib(n) {
-    const result = [0, 1];
-    for (let i = 2; i <= n; i++) {
-        const a = result[i-1];
-        const b = result[i-2];
+// //=== Officail01
+// function fib(n) {
+//     const result = [0, 1];
+//     for (let i = 2; i <= n; i++) {
+//         const a = result[i-1];
+//         const b = result[i-2];
 
-        result.push(a + b);
+//         result.push(a + b);
+//     }
+
+//     return result[n];
+// }
+
+//=== Officail02
+function fib(n) {
+    if ( n < 2) {
+        return n;
     }
 
-    return result[n];
+    return fib(n - 1) + fib(n - 2);
 }
 
 module.exports = fib;
