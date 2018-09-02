5c46a99ed4afaa1a1fa52bb5a679381843c60f44
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-09-02 10:19:58 +0800

    levelwidth -- by myself

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,27 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+// My Self
+function levelWidth(root) {
+    const level = [];
+    let counter = 0;
+    const arr = [root, true];
+    while (arr.length) {
+        const node = arr.shift();
+        if (node === true) {
+            level.push(counter);
+            counter = 0;
+            if (arr.length == 0) {
+                break;
+            } else { 
+                arr.push(true);
+                continue;
+            }
+        }
+        counter++;
+        arr.push(...node.children);
+    }
+    return level;
+}
 
 module.exports = levelWidth;
