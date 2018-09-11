27e01a9cf22a537b2110f387c0f2e6afeae85c10
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-09-11 14:18:25 +0800

    mergeSort --official

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -70,12 +70,25 @@ function selectionSort(arr) {
 // Myself
 function mergeSort(arr) {
     if (arr.length <= 1) {
-        return arr
+        return arr;
     }
-    
+
     let divIdx = Math.ceil(arr.length/2);
 
     return merge(mergeSort(arr.slice(0, divIdx)), mergeSort(arr.slice(divIdx, arr.length)))
+}
+
+// Official
+function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
+    
+    const center = Math.floor(arr.length / 2);
+    const left = arr.slice(0, center);
+    const right = arr.slice(center);
+
+    return merge(mergeSort(left), mergeSort(right));
 }
 
 // // Myself
