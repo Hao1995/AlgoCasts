4dee4aa3aa7e9a950619363927fe95c64d4b3c86
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-09-11 11:49:25 +0800

    selectionSort --official

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -30,19 +30,40 @@ function bubbleSort(arr) {
     return arr
 }
 
-// Myself
+// // Myself
+// function selectionSort(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         let indexofMin = i;
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (arr[j] < arr[indexofMin]) {
+//                 indexofMin = j; 
+//             }
+//         }
+//         const lesser = arr[indexofMin];
+//         arr[indexofMin] = arr[i];
+//         arr[i] = lesser;
+//     }
+//     return arr;
+// }
+
+// Official
 function selectionSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         let indexofMin = i;
-        for (let j = i + 1; j < arr.length; j++) {
+     
+        for (let j = i+1; j < arr.length; j++) {
             if (arr[j] < arr[indexofMin]) {
                 indexofMin = j; 
             }
         }
-        const lesser = arr[indexofMin];
-        arr[indexofMin] = arr[i];
-        arr[i] = lesser;
+
+        if (indexofMin !== i) {
+            let lesser = arr[indexofMin];
+            arr[indexofMin] = arr[i];
+            arr[i] = lesser;
+        }
     }
+
     return arr;
 }
 
