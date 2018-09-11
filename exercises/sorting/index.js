9e808aa9bdd5012512d4ab8bd56751d840a105fc
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-09-11 11:47:00 +0800

    selectionSort --myself

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -30,8 +30,20 @@ function bubbleSort(arr) {
     return arr
 }
 
+// Myself
 function selectionSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let indexofMin = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[indexofMin]) {
+                indexofMin = j; 
+            }
+        }
+        const lesser = arr[indexofMin];
+        arr[indexofMin] = arr[i];
+        arr[i] = lesser;
+    }
+    return arr;
 }
 
 function mergeSort(arr) {
